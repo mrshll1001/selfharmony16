18fe36a78dd5c6e7db57d4df89fcb229cc1bf7b8
Matt Marshall <matthew.mrshll@gmail.com>
2016-04-16 18:53:38 +0100

    JS

--- web/js/text-morph.js
+++ web/js/text-morph.js
@@ -1,3 +1,11 @@
-$('#person1-btn').click(function(e){
-  
+$('#person-0-btn').click(function(e){
+  alert('Person 0');
+});
+
+$('#person-1-btn').click(function(e){
+  alert('Person 1');
+});
+
+$('#person-2-btn').click(function(e){
+  alert('Person 2');
 });
